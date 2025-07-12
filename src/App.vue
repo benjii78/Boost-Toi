a5b4d311e3ab45bcbaf8b0cc687900a1a1cc730c
Benji Chounesse <ballaire1@myges.fr>
2025-07-12 18:52:38 +0200

    ajout de la configuration de supapase

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-container">
+    <nav class="navbar">
+      <RouterLink to="/">🏠 Accueil</RouterLink>
+      <RouterLink to="/suivi">📊 Suivi</RouterLink>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  background: #1f2937;
+  padding: 16px;
+  font-weight: bold;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+
+.app-container {
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/Suivi.vue
@@ -0,0 +1,8 @@
+<template>
+    <Progress />
+  </template>
+  
+  <script setup>
+  import Progress from '../components/Progress.vue';
+  </script>
+  
